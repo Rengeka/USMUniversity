c31e79312d763d707947504067f7c7b6b9ac60f7
Ciobanu <scioban1@techteam.com>
2025-11-03 11:01:56 +0200

    Added Cloud S3 sample app

---
+++ YearThree/Cloud/Lab4/SampleS3App/SampleS3App/Clients/S3Client.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.S3;
+using SampleS3App.Contracts;
+using Amazon.S3.Model;
+
+namespace SampleS3App.Clients;
+
+public class S3Client(IAmazonS3 s3Client) : IFileStorageClient
+{
+    private readonly string _bucketName = "sample";
+
+    public async Task PutFileAsync(IFormFile file, string key)
+    {
+        using var stream = file.OpenReadStream();
+        var request = new PutObjectRequest
+        {
+            BucketName = _bucketName,
+            Key = key,
+            InputStream = stream,
+            ContentType = file.ContentType
+        };
+
+        await s3Client.PutObjectAsync(request);
+    }
+
+    public async Task<Stream?> GetFileAsync(string key)
+    {
+        var response = await s3Client.GetObjectAsync(_bucketName, key);
+        return response.ResponseStream;
+    }
+
+    public async Task DeleteFileAsync(string key)
+    {
+        await s3Client.DeleteObjectAsync(_bucketName, key);
+    }
+}
---
+++ YearThree/Cloud/Lab4/SampleS3App/SampleS3App/Contracts/IFileStorageClient.cs
@@ -0,0 +1,11 @@
+﻿using Amazon.S3.Model;
+using SampleS3App.Clients;
+
+namespace SampleS3App.Contracts;
+
+public interface IFileStorageClient
+{
+    public Task PutFileAsync(IFormFile file, string key);
+    public Task<Stream?> GetFileAsync(string key);
+    public Task DeleteFileAsync(string key);
+}
